fd12cb9112534fed10f1a799cc2828504905eb99
Raúl Sevilla <rsevilla@redhat.com>
2019-06-08 20:12:52 +0200

    Configure hostname to VM at creation time

--- cli/vm/vm-configure.go
+++ cli/vm/vm-configure.go
@@ -11,19 +11,19 @@ import (
 const VIRTSYSPREP = "virt-sysprep"
 
 // disableCI Disables Cloud-init on the VM
-func (diskInfo *Disk) disableCI(args *[]string) {
+func (vm *VM) disableCI(args *[]string) {
 	*args = append(*args, "--touch")
 	*args = append(*args, "/etc/cloud/cloud-init.disabled")
 }
 
 // setRootPwd Sets root password on the VM
-func (diskInfo *Disk) setRootPwd(args *[]string, password string) {
+func (vm *VM) setRootPwd(args *[]string, password string) {
 	*args = append(*args, "--root-password")
 	*args = append(*args, fmt.Sprintf("password:%s", password))
 }
 
 // setPK Sets SSH public key to the given user of the VM
-func (diskInfo *Disk) setPK(args *[]string, user, pk string) error {
+func (vm *VM) setPK(args *[]string, user, pk string) error {
 	if _, err := os.Stat(pk); err != nil {
 		return err
 	}
@@ -33,7 +33,7 @@ func (diskInfo *Disk) setPK(args *[]string, user, pk string) error {
 }
 
 // setFB Creates a firstboot script for the VM
-func (diskInfo *Disk) setFB(args *[]string, script string) error {
+func (vm *VM) setFB(args *[]string, script string) error {
 	if _, err := os.Stat(script); err != nil {
 		return err
 	}
@@ -43,10 +43,12 @@ func (diskInfo *Disk) setFB(args *[]string, script string) error {
 }
 
 // customize Exexute virt-sysprep with the given customizations and relabels the system
-func (diskInfo *Disk) customize(args *[]string) error {
+func (vm *VM) customize(args *[]string) error {
+	*args = append(*args, "--hostname")
+	*args = append(*args, vm.Name)
 	*args = append(*args, "--selinux-relabel")
 	*args = append(*args, "-a")
-	*args = append(*args, diskInfo.Path)
+	*args = append(*args, vm.Disks[0].Path)
 	if err := cliutil.CmdExecutor(VIRTSYSPREP, *args); err != nil {
 		return err
 	}
